65895d6524b8a1d585aee090e1542a655d84e8a1
AFlazaroo <aflazaro@unbosque.edu.co>
2024-05-03 14:38:10 -0500

    Nuevo Proyecto BD Administración Bares

---
+++ src/main/java/co/edu/unbosque/BarApplication.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarApplication.class, args);
+
+	}
+
+}
